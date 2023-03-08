6ece4e3c99cd01fc3229e90899f87b1528aa7a62
Samuel Gonzalez <Nomekop65@yahoo.com>
2023-03-07 20:24:04 -0500

    Game loop now works, cleaned up a scoring bug, added dealer card visability

--- Program.cs
+++ Program.cs
@@ -80,121 +80,140 @@ namespace BlackJackCS
             var fullDeck = new List<Card>();
             var player1Hand = new Hand();
             var dealerHand = new Hand();
+            var playAgain = true;
 
-            //Uses nested loops to combine and store the strings
-            foreach (var suit in suits)
+            //               game loop should start here
+            while (playAgain)
             {
-                foreach (var rank in ranks)
+                //Uses nested loops to combine and store the strings
+                foreach (var suit in suits)
                 {
-                    fullDeck.Add(new Card(suit, rank));
-                }
-
-            }
-
-            //This code implements the Fisher-Yates shuffling algorithm
-            for (var rightIndex = fullDeck.Count - 1; rightIndex > 0; rightIndex--)
-            {
-                var randomNumberGenerator = new Random();
-                var randomNumber = randomNumberGenerator.Next(rightIndex);
-                var leftIndex = randomNumber;
-                var leftCard = fullDeck[leftIndex];
-                var rightCard = fullDeck[rightIndex];
-                fullDeck[leftIndex] = rightCard;
-                fullDeck[rightIndex] = leftCard;
-            }
-            //foreach (var card in fullDeck)
-            //{
-            //    Console.WriteLine($"{card.Rank} of {card.Suit} is worth {card.CardValue()} points");
-            //}
-            Console.WriteLine("Press any key to start the deal.");
-            Console.ReadLine();
-            // Deals first the dealer, then the player, two cards
-            dealerHand.Cards.Add(fullDeck[0]);
-            fullDeck.RemoveAt(0);
-            dealerHand.Cards.Add(fullDeck[0]);
-            fullDeck.RemoveAt(0);
-            Console.WriteLine($"Dealer drew two cards.");
-
-            player1Hand.Cards.Add(fullDeck[0]);
-            fullDeck.RemoveAt(0);
-            player1Hand.Cards.Add(fullDeck[0]);
-            fullDeck.RemoveAt(0);
-            foreach (var card in player1Hand.Cards)
-            {
-                Console.WriteLine($"You drew the {card.CardDisplay()}");
-            }
-            Console.WriteLine($"Your hand is worth {player1Hand.HandValue()}");
-
-            //loop for player choices
-            var keepGoing = true;
-            while (keepGoing)
-            {
-                Console.WriteLine("Would you like to (s)tay or (h)it?");
-                var stayOrHit = Console.ReadLine();
-                if (stayOrHit == "h")
-                {
-                    player1Hand.Cards.Add(fullDeck[0]);
-                    Console.WriteLine($"You drew the {fullDeck[0].CardDisplay()}");
-                    fullDeck.RemoveAt(0);
-                    Console.WriteLine($"Your hand is now worth {player1Hand.HandValue()}.");
-                    if (player1Hand.HandValue() > 21)
-                    { keepGoing = false; }
-                }
-                else if (stayOrHit == "s")
-                {
-                    keepGoing = false;
-                }
-                else
-                {
-                    Console.WriteLine("Please choose to stay or hit.");
-                }
-            }
-            //check for bust
-            var playerBust = false;
-            var dealerBust = false;
-            if (player1Hand.HandValue() > 21)
-            {
-                Console.WriteLine("You busted!");
-                playerBust = true;
-            }
-
-            //loop for dealer choices
-            else
-            {
-                if (dealerHand.HandValue() > 21)
-                {
-                    Console.WriteLine($"The dealer has {dealerHand.HandValue()} points and busted!");
-                    dealerBust = true;
-                }
-                else if (dealerHand.HandValue() < player1Hand.HandValue())
-                {
-                    while (dealerHand.HandValue() < 17)
+                    foreach (var rank in ranks)
                     {
-                        Console.WriteLine($"The dealer's hand is at {dealerHand.HandValue()} points.");
-                        Console.WriteLine($"The dealer draws the {fullDeck[0].CardDisplay()}");
-                        dealerHand.Cards.Add(fullDeck[0]);
+                        fullDeck.Add(new Card(suit, rank));
+                    }
+
+                }
+
+                //This code implements the Fisher-Yates shuffling algorithm
+                for (var rightIndex = fullDeck.Count - 1; rightIndex > 0; rightIndex--)
+                {
+                    var randomNumberGenerator = new Random();
+                    var randomNumber = randomNumberGenerator.Next(rightIndex);
+                    var leftIndex = randomNumber;
+                    var leftCard = fullDeck[leftIndex];
+                    var rightCard = fullDeck[rightIndex];
+                    fullDeck[leftIndex] = rightCard;
+                    fullDeck[rightIndex] = leftCard;
+                }
+                //foreach (var card in fullDeck)
+                //{
+                //    Console.WriteLine($"{card.Rank} of {card.Suit} is worth {card.CardValue()} points");
+                //}
+                Console.WriteLine("Press any key to start the deal.");
+                Console.ReadLine();
+                // Deals first the dealer, then the player, two cards
+                dealerHand.Cards.Add(fullDeck[0]);
+                fullDeck.RemoveAt(0);
+                dealerHand.Cards.Add(fullDeck[0]);
+                fullDeck.RemoveAt(0);
+                Console.WriteLine($"Dealer drew two cards.");
+
+                player1Hand.Cards.Add(fullDeck[0]);
+                fullDeck.RemoveAt(0);
+                player1Hand.Cards.Add(fullDeck[0]);
+                fullDeck.RemoveAt(0);
+                foreach (var card in player1Hand.Cards)
+                {
+                    Console.WriteLine($"You drew the {card.CardDisplay()}");
+                }
+                Console.WriteLine($"Your hand is worth {player1Hand.HandValue()}");
+
+                //loop for player choices
+                var keepGoing = true;
+                while (keepGoing)
+                {
+                    Console.WriteLine("Would you like to (s)tay or (h)it?");
+                    var stayOrHit = Console.ReadLine();
+                    if (stayOrHit == "h")
+                    {
+                        player1Hand.Cards.Add(fullDeck[0]);
+                        Console.WriteLine($"You drew the {fullDeck[0].CardDisplay()}");
                         fullDeck.RemoveAt(0);
+                        Console.WriteLine($"Your hand is now worth {player1Hand.HandValue()}.");
+                        if (player1Hand.HandValue() > 21)
+                        { keepGoing = false; }
+                    }
+                    else if (stayOrHit == "s")
+                    {
+                        keepGoing = false;
+                    }
+                    else
+                    {
+                        Console.WriteLine("Please choose to stay or hit.");
                     }
                 }
-            }
-            Console.WriteLine($"The dealer's hand is at {dealerHand.HandValue()} points.");
+                //check for bust
+                var playerBust = false;
+                var dealerBust = false;
+                if (player1Hand.HandValue() > 21)
+                {
+                    Console.WriteLine("You busted!");
+                    playerBust = true;
+                }
 
-            //compare totals and check for winner
-            if (playerBust)
-            {
-                Console.WriteLine($"Your hand had {player1Hand.HandValue()} points and is a bust. You lose!");
-            }
-            else if (dealerBust)
-            {
-                Console.WriteLine($"The dealer hand had {dealerHand.HandValue()} points and was a bust. You win!");
-            }
-            else if (player1Hand.HandValue() > dealerHand.HandValue())
-            {
-                Console.WriteLine("Your hand beat the dealer's hand. You win!");
-            }
-            else if (player1Hand.HandValue() <= dealerHand.HandValue())
-            {
-                Console.WriteLine("The dealer hand beats yours. You lose!");
+                //loop for dealer choices
+                else
+                {
+                    Console.WriteLine($"The dealer reveals {dealerHand.Cards[0].CardDisplay()} and {dealerHand.Cards[1].CardDisplay()}");
+                    if (dealerHand.HandValue() > 21)
+                    {
+                        Console.WriteLine($"The dealer has {dealerHand.HandValue()} points and busted!");
+                        dealerBust = true;
+                    }
+                    else if (dealerHand.HandValue() < player1Hand.HandValue())
+                    {
+                        while (dealerHand.HandValue() < 17)
+                        {
+                            Console.WriteLine($"The dealer's hand is at {dealerHand.HandValue()} points.");
+                            Console.WriteLine($"The dealer draws the {fullDeck[0].CardDisplay()}");
+                            dealerHand.Cards.Add(fullDeck[0]);
+                            fullDeck.RemoveAt(0);
+                        }
+                    }
+                }
+                Console.WriteLine($"The dealer's hand is at {dealerHand.HandValue()} points.");
+                if (dealerHand.HandValue() > 21)
+                {
+                    dealerBust = true;
+                }
+                //compare totals and check for winner
+                if (playerBust)
+                {
+                    Console.WriteLine($"Your hand had {player1Hand.HandValue()} points and is a bust. You lose!");
+                }
+                else if (dealerBust)
+                {
+                    Console.WriteLine($"The dealer hand had {dealerHand.HandValue()} points and was a bust. You win!");
+                }
+                else if (player1Hand.HandValue() > dealerHand.HandValue())
+                {
+                    Console.WriteLine("Your hand beat the dealer's hand. You win!");
+                }
+                else if (player1Hand.HandValue() <= dealerHand.HandValue())
+                {
+                    Console.WriteLine("The dealer hand beats yours. You lose!");
+                }
+                //               game loop ends here, need to clear out deck, player hand, and dealer hand
+                Console.WriteLine("Play again? Type (y) to play or anything else to quit");
+                var decideToPlayAgain = Console.ReadLine();
+                if (decideToPlayAgain != "y")
+                {
+                    playAgain = false;
+                }
+                fullDeck.Clear();
+                player1Hand.Cards.Clear();
+                dealerHand.Cards.Clear();
             }
         }
     }
